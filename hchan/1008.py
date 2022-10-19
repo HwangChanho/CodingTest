b80ba09c68562d154c370b4f6f1bbb373b0a9dbc
Alex_Hwang <cksgh0316@gmail.com>
2022-10-19 16:01:40 +0900

    test

--- hchan/1008.py
+++ hchan/1008.py
@@ -1,5 +1,8 @@
 # https://www.acmicpc.net/submit/1008/40889532
 
 a, b = map(int, input().split())
+a, b = map(int, input().split())
+a, b = map(int, input().split())
+a, b = map(int, input().split())
 
 print(a / b)
